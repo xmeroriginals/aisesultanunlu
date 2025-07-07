cec5b507ed00da86596b40f264fe517de8e962a5
XmerOriginals <108153683+xmeroriginals@users.noreply.github.com>
2025-07-07 16:58:07 +0300

    + Small Update

--- src/task.js
+++ src/task.js
@@ -210,3 +210,11 @@ function showNextHeart() {
 }
 
 setInterval(showNextHeart, 1700);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const avatar = document.getElementById("avatar");
+  const avatars = ["./assets/mem/asu.jpeg", "./assets/mem/asun.jpg"];
+
+  const randomIndex = Math.floor(Math.random() * avatars.length);
+  avatar.src = avatars[randomIndex];
+});
